f11eeda314a7c1080f684ecbee5372a52534665c
michelwene <michelwene@hotmail.com>
2023-01-27 14:25:14 -0300

    feat: auth user

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./modules/account/autheticateUser/AuthenticateUserController";
 import { CreateUserController } from "./modules/users/useCases/createUser/CreateUserController";
 
 const routes = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
-routes.post("/users/auth", createUserController.handle);
+routes.post("/users", createUserController.handle);
+routes.post("/users/auth", authenticateUserController.handle);
 
 export { routes };
