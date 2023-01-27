7e2b070d1c3173990a94212727ca1c7a0e899ff3
michelwene <michelwene@hotmail.com>
2023-01-27 14:21:57 -0300

    feat: auth user

---
+++ src/modules/account/autheticateUser/AuthenticateUserUseCaso.ts
@@ -0,0 +1,34 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { prisma } from "../../../database/prismaClient";
+
+interface IAuthenticateUser {
+  email: string;
+  password: string;
+}
+
+export class AuthenticaUserUseCase {
+  async execute({ password, email }: IAuthenticateUser) {
+    const client = await prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!client) {
+      throw new Error("Usuário com este email não existe");
+    }
+
+    const passwordMath = await compare(password, client.password);
+
+    if (!passwordMath) {
+      throw new Error("Senha incorreta!");
+    }
+
+    const token = sign({ email }, "e87dc0aa180c34b3f273b334b32c18a3", {
+      subject: client.id,
+      expiresIn: "1d",
+    });
+    return token;
+  }
+}
