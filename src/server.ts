d77189d089d2a449e46e2cfc83333916e9dbea78
Michel Wene <michelwene@hotmail.com>
2023-02-02 16:22:56 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-  (request: Request, response: Response, error: Error, next: NextFunction) => {
+  (request: Request, response: Response, error: any, next: NextFunction) => {
     if (error instanceof Error) {
       response.status(400).json({
         message: error.message,
